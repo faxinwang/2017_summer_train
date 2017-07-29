baf7d5543176b3666b0fd20f1bc04e1ab2dd4155
faxinwang <www.wjx.gg.66@qq.com>
2017-07-29 21:37:36 +0800

    add file 8,9 高斯消元, 莫比乌斯

---
+++ 8.高斯消元-平方数.cpp
@@ -0,0 +1,135 @@
+/*
+题目内容：
+ 有n个整数a1,a2,...an，每个数ai的质因子不超过2000. 现从中选取一些数，它们的乘积刚好是平方数。
+问有多少种挑选方案。 假定n<=300, ai<=2000
+
+输入描述
+第一行是n
+第二行是n个整数
+
+输出描述
+方案数。为防止数目过大，结果需模1000000007.
+
+输入样例
+3
+3 3 4
+
+输出样例
+3
+*/
+
+/*
+解题思路:
+	对于任意自然数n,都可以将它分解为n=P1^R1 * P2^R2 * P3^R3....,(其中Pi为其质因数,Ri为Pi的指数), 
+	该式子就叫n的 "唯一分解式". 
+	对于平方数N,它的唯一分解式 N = P1^R1 * P2^R1 + P3^R3+.....中的Ri必须全都为偶数,这样开方出
+	来的才是整数,基于这一性质, 我们对给定的每个整数a1,a2,...,an进行质因数分解,并记录所有整数ai
+	的所有质因数Pi的个数Ri,并保存在一个二维矩阵中,如下:
+P1	R11*x1 	R12*x2 	R13*x3 ...  R1n*xn
+P2  R21*x1	R22*x2	R23*x3 ...	R2n*xn
+...	...
+Pj  Ri1*x1  ...		Rij*xi ...  Rin*xn
+... ... 
+Pm	Rm1*x1	Rm1*x2	Rm3*x3 ...  Rmn*xn
+	其中: 
+	1) P1,P2,...,Pm为a1,a2,...,an分解出来的所有的质数.
+	2) R12为a2分解出来的质数P1的个数,Rij为aj分解出来的质数pi的个数 
+	3) xi取值0或1,取1表示选择ai,取0表示不选择ai
+要使得最后选出的数的乘积N是一个完全平方数,则式N = P1^R1 * P2^R1 + P3^R3+...+PmRm中的每个质因数Pi
+对应的指数Ri必须为偶数,所以可以得到如下方程:
+	R11*x1 + R12*x2 + R13*x3 + ... + R1n*xn = R1
+	R21*x1 + R22*x2	+ R23*x3 + ... + R2n*xn = R2
+	...
+	Rm1*x1 + Rm2*x2 + Rm3*x3 + ... + Rmn*xn = Rn
+	其中:
+	1) 因为xi的取值为0或1,所以Ri的值即为选出的所有整数{xi}分解出的质数Pi的总个数.
+	2) 上面已经说了, 要使得最后乘积N为完全平方数,必须满足Ri均为偶数
+由于我们只需要记录Rij和Ri是奇数还是偶数,所以我们可以将上述方程进一步简化, 
+	R11*x1 + R12*x2 + R13*x3 + ... + R1n*xn = 0 (mod 2) 
+	R21*x1 + R22*x2	+ R23*x3 + ... + R2n*xn = 0 (mod 2) 
+	...
+	Rm1*x1 + Rm2*x2 + Rm3*x3 + ... + Rmn*xn = 0 (mod 2)
+这样,xi的系数Rij就都是0或1的值, 并且方程的右边全都变成了0(之前Ri必须都为偶数才能使选出的数的成绩为
+完全平方数)这进一步简化了后面高斯消元的过程. 
+对于这个方程,我们把系数用二维数组保存起来,然后用高斯消元法求出该矩阵的秩r,进而可得该方程组的自由变
+元的个数n-r. 由于xi的取值只有0或1两种选择, 所以总的满足条件的选择方案数就是2^(n-r) - 1,(减一是因为
+要减去一个都不选的情况,因为题目要求至少选择一个正整数)
+*/
+
+#include <iostream>
+#include<cstdio>
+using namespace std;
+const int Max = 2005;
+const int Mod = 1000000007;
+bool vis[Max];
+char mtx[500][305];
+int  p[500]; //初步估计小于2000的质数不超过1000个
+
+int init(){
+	int cnt=0;
+	for(int i=2; i<Max; ++i){
+		if(!vis[i]){
+			p[cnt++] = i;
+			for(int j=i*i; j<=Max; j+=i) vis[j] = 1;
+		}
+	}
+	return cnt;
+}
+
+//计算a^n % Mod 
+int quick_mod(int a,int n){
+	int ans = 1, x = a % Mod;
+	while(n){
+		if(n&1) ans = ans * x % Mod;
+		x = x * x % Mod;
+		n >>= 1;
+	}
+	return ans;
+}
+
+//高斯消元 
+int rank(char a[][305], int m , int n )  
+{  
+    int i = 0 , j = 0 , k , r , u;  
+    while( i < m && j < n )  
+    {  
+        r = i;  
+        for( k = i; k < m; k++ )  
+            if ( a[k][j] ) {r = k;break;}  
+        if ( a[r][j] )  
+        {  
+            if ( r != i )  
+                for( k = 0; k <= n; k++ ) swap( a[r][k] , a[i][k] );  
+            for( u = i + 1; u < m; u++ )  
+                if ( a[u][j] )  
+                for( k = i; k <= n; k++ )   a[u][k] ^= a[i][k];  
+            i++;  
+        }  
+        j++;  
+    } 
+    return i;  
+}  
+
+int main(){
+#ifdef WFX
+freopen("8 in.txt","r",stdin);
+#endif
+	int n,a,maxp=0;
+	int prime_n = init(); //找出所有不大于2000的质数并返回个数 
+	scanf("%d",&n);
+	for(int i=0;i<n;++i){
+		scanf("%d",&a);
+		//对a进行质因数分解,结果保存在mtx矩阵中 
+		for(int j=0;j<prime_n && a;++j){
+			while(a % p[j] == 0){
+				a /= p[j];
+				mtx[j][i] ^= 1; //与1异或,效果等于(mtx[j][i]+1)%2
+				//记录分解出的最大的质因数在p数组中的下标 
+				if(maxp < j) maxp = j;
+			}
+		}
+	}
+	int r = rank(mtx,maxp+1,n); //返回矩阵的秩 
+	printf("%d",quick_mod(2,n - r) -1);
+	return 0; 
+} 
---
+++ 9.莫比乌斯-gcd配对数.cpp
@@ -0,0 +1,123 @@
+/*
+题目内容：
+给你5个整数a,b,c,d,k, 求满足a <= x <= b && c <= y <= d && gcd(x,y)=k 的数对(x,y)的对数。
+规定（x,y）和（y,x）不重复计数。
+输入描述
+第一行输入测试用例的个数n, 接下来是n行测试用例
+每行测试用例包括a b c d k共5个数，其中 0 < a <= b <= 100,000,
+ 0 < c <= d <= 100,000, 0 <= k <= 100,000, 
+
+（为了简单起见，可以假设a=c=1）
+
+输出描述
+输出n行， 每行是一个测试用例中的对数。
+
+输入样例
+2
+1 3 1 5 1
+1 11014 1 14409 9
+
+输出样例
+9
+736427
+*/
+
+/*
+参考 http://blog.csdn.net/lixuepeng_001/article/details/50577932 
+
+解题思路:
+看了很多博客才慢慢搞懂了. 下面说一下自己的理解. 
+首先看一下莫比乌斯函数mu(x)的定义: 
+1) 若 x = 1, 则 mu(x) = 1
+2) 若 x = P1*P2* ... * Pk,(Pi均为质数) 则 mu(x)=pow(-1,k)
+3) 其他情况下, mu(x) = 0 
+
+对莫比乌斯函数,有两个反演公式:
+F(n) = Sum{ (d|n)| f(d) }  ---->  f(n) = Sum{ (d|n)| F(n/d)*mu(d) }
+F(n) = Sum{ (n|d)| f(d) }  ---->  f(n) = Sum{ (n|d)| F(d)*mu(d/n) }
+第一种:就是说如果函数F(n)等于所有f(d)的和(其中d为n所有的约数),
+则f(n)等于所有F(n/d)*mu(d)的和. 
+第二种: 就是说如果函数F(n)等于所有f(d)的和(其中d是n的倍数),
+则f(n)等于所有F(d/n)*mu(d/n)的和(本题使用的正式这个公式). 
+
+既然这题可以用莫比乌斯反演, 那我们就需要构造出符合这种条件的F()函数和f()函数:
+定义: 
+对a <= x <= b, c <= y <= d:
+f(n) 为 gcd(x,y)=n 的数对(x,y)的数目 
+F(n) 为 gcd(x,y)=k*n 的数对(x,y)的数目(k=1,2,3,...)
+则 F(n) = Sum{ (n|d)| f(d) }, 从而得到 f(n) = Sum{ (n|d) | F(d)*mu(d/n) }
+那么F(n)怎么算呢?
+在区间[a,b]内, x可取的值的个数有(b-a+1)/n个, y可取的值的个数有(d-c+1)/n个 
+所以F(n) = (b-a+1)/n * (d-c+1)/n;
+本题中a,c为1,所以F(x) = (m/x)*(n/x),  (m=b/k,n=d/k)
+所以f(1) = mu(1)*F(1) + mu(2)*F(2) + ... + mu(min(m,n))*F(min(m,n))
+
+然后分析一下题目:
+求1 <= x <= b, 1 <= y <= d中满足gcd(x,y)==k的数对(x,y)的数目,问题可以转化为
+求1 <= x <= b/k, 1 <= y <= d/k中满足gcd(x/k, y/k)=1 的数对 (x,y)的数目,这不正好就是f(1)的值吗.
+
+接下来只需要去重就可以了,因为题目说(x,y)和(y,x)不重复计算.
+G(b,d)- G(b,b) / 2 
+
+*/
+
+#include<iostream>
+#include<cstdio>
+using namespace std;
+typedef long long LL;
+const int maxn = 1e5+5;
+int mu[maxn],p[maxn];
+bool vis[maxn];
+
+/* 
+莫比乌斯函数筛:
+若i为奇数个不同素数之积mu[i] = -1
+若i为偶数个不同素数之积mu[i] = 1
+若i有平方因子则mu[i] = 0。
+这个做起来比欧拉函数容易，在素数筛上，若i为素数则mu[i] = -1，若i % p[j] == 0，
+ 则mu[i * p[j]] = 0，显然p[j]就是它的平方因子，否则mu[i * p[j]] = -mu[i]
+*/  
+void init_mu(){
+	int cnt=0;
+	mu[1]=1;
+	for(int i=2;i<maxn; ++i){
+		if(!vis[i]){
+			p[cnt++] = i;
+			mu[i] = -1;
+		}
+		for(int j=0; j<cnt && i*p[j]<maxn; ++j){
+			vis[i*p[j]] = 1;
+			if(i%p[j] == 0){
+				mu[i*p[j]] = 0;
+				break;
+			}
+			mu[i*p[j]] = -mu[i];
+		}
+	}
+}
+
+
+int main(){
+#ifdef WFX
+freopen("9 in.txt","r",stdin);
+#endif
+	int T,a,b,c,d,k;
+	init_mu();
+	scanf("%d",&T);
+	while(T--){
+		scanf("%d%d%d%d%d",&a,&b,&c,&d,&k);
+		if(k==0){
+			printf("0"); continue;
+		}
+		b/=k, d/=k;
+		LL cnt1=0,cnt2=0;
+		LL lim = b < d ? b : d;
+		for(int i=1; i<=lim; ++i){
+			cnt1 += mu[i]*(b/i)*(d/i);
+			cnt2 += mu[i]*(lim/i)*(lim/i);
+		}
+		printf("%d\n",cnt1-cnt2/2);
+	}
+	
+	return 0;
+}
